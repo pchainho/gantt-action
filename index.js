78f562293adf56d83d47ac45ccf0bd8dd4ad5427
Paulo Chainho <pchainho@gmail.com>
2022-11-12 23:42:31 +0000

    update

--- index.js
+++ index.js
@@ -17,25 +17,25 @@ function getInputs() {
   };
 }
 
+function getInputs(milestones) {
+  milestones.forEach(milestone => {
+    console.log(milestone.title);
+    console.log(milestone.description.starts);
+    console.log(milestone.due_on);
+    console.log(milestone.closed_issues/(milestone.open_issues+milestone.closed_issues*100));
+
+  });
+
+}
+
 async function run() {
   try {
-    // `who-to-greet` input defined in action metadata file
-    /*const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);*/
-
     // Octokit.js
     // https://github.com/octokit/core.js#readme
 
   const { repo, owner, token } = getInputs();
-  console.log(owner);
-  console.log(repo);
-  const octokit = github.getOctokit(token);
 
+  const octokit = github.getOctokit(token);
 
     const response = await octokit.request(
       `GET /repos/{owner}/{repo}/milestones`,
